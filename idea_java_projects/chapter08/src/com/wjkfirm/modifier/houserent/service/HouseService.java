02c8fb5c8b011b913a548c17c76bbefe5c202524
jyuankai <aniceguy16@gmail.com>
2021-08-30 00:39:03 +0800

    21/08/29

--- idea_java_projects/chapter08/src/com/wjkfirm/modifier/houserent/service/HouseService.java
+++ idea_java_projects/chapter08/src/com/wjkfirm/modifier/houserent/service/HouseService.java
@@ -16,15 +16,12 @@ public class HouseService {
     private int houseID = 1;//將對應ID編進houseID中
 
     public HouseService(int size) {
-
         houses = new House[size];//當宣告HouseService的時候指定數組大小
-
         //為了測試，先初始化一筆資料
         houses[0] = new House(0, "初始資料", "113", "台北市", 7000, "未出租");
-
     }
 
-    //add方法，新增新的資料到house數組中，返回boolean
+    //1. add方法，新增新的資料到house數組中，返回boolean
     public boolean add(House newHouse) {
         if (houseNum == houses.length) {//數組已滿，不能再新增了
             System.out.println("數量已滿，無法再新增新的房屋...");
@@ -39,10 +36,19 @@ public class HouseService {
         houseNum++;
         return true;
     }
-
-    //del方法，刪除一個房屋資料
+    //2. find方法，尋找房屋ID並顯示該房屋資料
+    public House find(int findID){
+        //如果找到ID號碼，則直接回傳houses[i]
+        for (int i = 0; i < houseNum; i++) {
+            if (findID == houses[i].getId()){
+             return houses[i];
+            }
+        }
+        //如果沒有找到直接回傳空值給View做判斷
+        return null;
+    }
+    //3. del方法，刪除一個房屋資料
     public boolean del(int delId) {
-
         int index = -1;
         //尋找目前所有的houses數量
         for (int i = 0; i < houseNum; i++) {
@@ -50,13 +56,10 @@ public class HouseService {
                 index = i;
             }
         }
-
         //如果沒找到退出迴圈
         if (index == -1) {
-
             return false;
         }
-
         //確定要刪除的houses[i]後，將要刪除的資料用後一筆資料取代，並將最後一組資料設為空值null
         for (int i = index; i < houseNum; i++) {
             houses[i] = houses[i+1];
@@ -65,13 +68,19 @@ public class HouseService {
         houseNum--;
 
         return true;
-
     }
-
-    public House[] list() {//使用此方法回傳houses裡儲存的資料
-
+    //4. 修改房屋訊息
+    public House update(int updateId){
+        //尋找目前所有的houses數量
+        for (int i = 0; i < houseNum; i++) {
+            if (houses[i].getId() == updateId) {
+                return houses[i];
+            }
+        }
+        return null;
+    }
+    //使用此方法回傳houses裡儲存的資料
+    public House[] list() {
         return houses;
-
     }
-
 }
--- idea_java_projects/chapter08/src/com/wjkfirm/modifier/houserent/view/HouseView.java
+++ idea_java_projects/chapter08/src/com/wjkfirm/modifier/houserent/view/HouseView.java
@@ -12,7 +12,6 @@ import java.util.Scanner;
  */
 
 public class HouseView {
-
     Scanner scanner = new Scanner(System.in);
 
     private boolean loop = true;
@@ -21,8 +20,7 @@ public class HouseView {
 
     //1.接收新增的房屋資料，接收用戶輸入的方屋資料，調用service的add方法
     public void addHouse() {
-
-        System.out.println("==================新增房屋==================");
+        System.out.println("==================新增房屋==================\n");
         System.out.println("姓名: ");
         String name = scanner.next();
         System.out.println("電話: ");
@@ -34,51 +32,99 @@ public class HouseView {
         System.out.println("狀態: ");
         String state = scanner.next();
 
-
         //新增一個House對象，ID是系統給的，無法自訂。
         House newHouse = new House(0, name, "\t" + phone, address + "\t", rent, state);
 
-
         //已將newHouse傳入HouseService中後加入houses數組中，等service判斷是否成功回傳T/F
         if (houseService.add(newHouse)) {
-
             System.out.println("==================新增成功==================\n");
-
         } else {
-
             System.out.println("==================新增失敗==================\n");
-
         }
-
-
     }
-
-    //2.刪除房屋資料，接收要刪除ID號碼，調用service的del方法
+    //2.房屋搜尋功能
+    public void findHouse() {
+        //請使用主輸入要選擇的房屋編號，輸入-1則退出。
+        System.out.println("==================搜尋房屋==================\n");
+        System.out.println("請輸入要搜尋的房屋編號(-1 退出):");
+        int findID = scanner.nextInt();
+        char check = ' ';
+        if (findID == -1) {
+            System.out.println("==================取消搜尋==================\n");
+            return;
+        }
+        //將搜尋編號ID傳入find方法中搜尋，如果找到則回傳資料houses[i]，沒找到回傳空值
+        House returnValue = houseService.find(findID);
+        if (returnValue == null) {
+            System.out.println("================無效的房屋編號================\n");
+        } else {//將找到的結果顯示出來
+            System.out.println("================以下是搜尋結果================\n");
+            System.out.println("編號\t\t房主\t\t電話\t\t地址\t\t月租\t\t狀態(未出租/已出租)");
+            System.out.println(returnValue);
+        }
+    }
+    //3.刪除房屋資料，接收要刪除ID號碼，調用service的del方法
     public void delHouse() {
 
-        System.out.println("==================刪除房屋==================");
-        System.out.println("請輸入要刪除的房屋編號(-1退出):");
+        System.out.println("==================刪除房屋==================\n");
+        System.out.println("請輸入要刪除的房屋編號(-1 退出):");
         int delID = scanner.nextInt();
         char check = ' ';
         if (delID == -1) {
-            System.out.println("=================取消刪除房屋=================");
+            System.out.println("=================取消刪除房屋=================\n");
             return;
         }
         System.out.println("請確認是否要刪除編號 " + delID + " (y/n):");
         check = scanner.next().charAt(0);
         if (check == 'y') {
             if (houseService.del(delID)) {
-                System.out.println("=================成功刪除房屋=================");
+                System.out.println("=================成功刪除房屋=================\n");
             } else {
-                System.out.println("================無效的房屋編號================");
+                System.out.println("================無效的房屋編號================\n");
+                return;
             }
         } else {
-            System.out.println("=================取消刪除房屋=================");
+            System.out.println("=================取消刪除房屋=================\n");
+            return;
         }
-
     }
+    //4.修改房屋訊息
+    public void update() {
+        System.out.println("=================修改房屋列表=================\n");
+        System.out.println("請選擇要修改的房屋編號(-1退出)");
+        int updateId = scanner.nextInt();
+        if (updateId == -1) {
+            System.out.println("==================取消修改==================\n");
+            return;
+        }
+        House house = houseService.update(updateId);
 
+        if (house != null) {//回傳的資料如果是null則表示輸入的資料錯誤
+            System.out.println("請輸入要修改的資料");
+            System.out.print("姓名:(" + house.getName() + ")");
+            String name = scanner.next();
+            house.setName(name);
 
+            System.out.println("電話:(" + house.getPhone() + ")");
+            String phone = scanner.next();
+            house.setPhone(phone);
+
+            System.out.println("地址:(" + house.getAddress() + ")");
+            String address = scanner.next();
+            house.setAddress(address);
+
+            System.out.println("月租:(" + house.getRent() + ")");
+            int rant = scanner.nextInt();
+            house.setRent(rant);
+
+            System.out.println("狀態:(" + house.getState() + ")");
+            String state = scanner.next();
+            house.setState(state);
+        } else {
+            System.out.println("================無效的房屋編號================\n");
+            return;
+        }
+    }
     //5.顯示房屋列表
     public void listHouses() {
         System.out.println("==================房屋列表==================\n");
@@ -88,20 +134,21 @@ public class HouseView {
             if (houses[i] == null) {
                 break;
             }
-
             System.out.println(houses[i]);
-
         }
-
+    }
+    //6.退出程式
+    public void Exit() {
+        System.out.println("請確認是否要退出程式(y/n):");
+        char c = scanner.next().charAt(0);
+        if (c == 'y') {
+            loop = false;
+        }
     }
 
-
     //main.顯示主菜單
-
     public void mainMenu() {
-
         do {
-
             System.out.println("==================房屋出租系統菜單==================");
             System.out.println("\t\t\t\t  1 新 增 房 源");
             System.out.println("\t\t\t\t  2 搜 尋 房 屋");
@@ -113,40 +160,25 @@ public class HouseView {
             key = scanner.next().charAt(0);
 
             switch (key) {
-
                 case '1':
                     addHouse();
                     break;
-
                 case '2':
-                    System.out.println("2 搜 尋 房 屋");
+                    findHouse();
                     break;
-
                 case '3':
                     delHouse();
                     break;
-
                 case '4':
-                    System.out.println("4 修 改 房 屋 訊 息");
+                    update();
                     break;
-
                 case '5':
                     listHouses();
                     break;
-
                 case '6':
-                    System.out.println("6 退      出");
-                    loop = false;
+                    Exit();
                     break;
-
-
             }
-
-
         } while (loop);
-
-
     }
-
-
 }
